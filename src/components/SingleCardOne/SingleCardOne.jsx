08707f987aed784224fc33df59400c98647f45e6
Rinku Hasija <rishabhasija5454@gmail.com>
2023-03-06 22:40:38 +0530

    fix-vercel

--- src/components/SingleCardOne/SingleCardOne.jsx
+++ src/components/SingleCardOne/SingleCardOne.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import logo from '../../assets/image-1.png'
+// import React from 'react'
+// import logo from '../../assets/image-1.png'
 
-function SingleCardOne() {
-    return (
+// function SingleCardOne() {
+//     return (
 
-        <React.Fragment>
+//         <React.Fragment>
 
-            <div onClick={handleClick} className={classnames(styles.card, styles.cardOne, styles.cardActive)}>
-                <p> Action </p>
+//             <div onClick={handleClick} className={classnames(styles.card, styles.cardOne, styles.cardActive)}>
+//                 <p> Action </p>
 
-                <div className={classnames(styles.imgBack, styles.imgActive)}>
+//                 <div className={classnames(styles.imgBack, styles.imgActive)}>
 
-                    <img width='180px' src={logo} alt='image-1'></img>
+//                     <img width='180px' src={logo} alt='image-1'></img>
 
-                </div>
+//                 </div>
 
-            </div>
+//             </div>
 
-        </React.Fragment>
+//         </React.Fragment>
 
-    )
-}
+//     )
+// }
 
-export default SingleCardOne
+// export default SingleCardOne
